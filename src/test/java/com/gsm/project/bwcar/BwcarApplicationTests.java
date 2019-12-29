8a630e38478e232ad8a778495eb383d397a74dfd
hms861972 <674948169@qq.com>
2019-12-30 00:21:43 +0800

    第一次上传
    Signed-off-by: hms861972 <674948169@qq.com>

---
+++ src/test/java/com/gsm/project/bwcar/BwcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsm.project.bwcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwcarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
