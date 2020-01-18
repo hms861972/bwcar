6d4fb5dcb9f5202d1898cf0f861b2157d73d1b71
hms861972 <674948169@qq.com>
2020-01-18 17:00:52 +0800

    map结尾上传
    Signed-off-by: hms861972 <674948169@qq.com>

--- src/main/java/com/gsm/project/bwcar/BwcarApplication.java
+++ src/main/java/com/gsm/project/bwcar/BwcarApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.gsm.project.bwcar.dao")
-@ServletComponentScan(basePackages="com.gsm.project.bwcar.config")
+@ServletComponentScan(basePackages = "com.gsm.project.bwcar.config")
 public class BwcarApplication {
     public static void main(String[] args) {
         SpringApplication.run(BwcarApplication.class, args);
