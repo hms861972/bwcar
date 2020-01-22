3b1e67b182447bd619238ab171e8683fbdafc91e
hms861972 <674948169@qq.com>
2020-01-22 21:33:45 +0800

    map结尾上传
    Signed-off-by: hms861972 <674948169@qq.com>

--- src/main/java/com/gsm/project/bwcar/exception/RException.java
+++ src/main/java/com/gsm/project/bwcar/exception/RException.java
@@ -1,5 +1,36 @@
 package com.gsm.project.bwcar.exception;
 
 public class RException extends RuntimeException {
+    private int code;
+    private String msg;
 
+    public RException() {
+    }
+
+    public RException(int code, String msg) {
+        super(msg);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public RException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
---
+++ src/main/java/com/gsm/project/bwcar/exception/RExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.gsm.project.bwcar.exception;
+
+import com.gsm.project.bwcar.utils.R;
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.naming.AuthenticationException;
+
+@ControllerAdvice
+public class RExceptionHandler {
+
+    @ExceptionHandler(RException.class)
+    @ResponseBody
+    public R rExp(RException rException){
+        R r = new R();
+        r.put("code",500);
+        r.put("msg",rException.getMsg());
+        return r;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R allExp(Exception exception){
+        R r = new R();
+        r.put("code",500);
+        r.put("msg","系统内部异常请联系管理员");
+        return r;
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public String allExp(AuthorizationException authorizationException){
+        return "redirect:static/unauthorized.html";
+    }
+}
